d0fc5598fbae45c6fb33416d889eb3f1402f6785
Finn <36242395+ploorp@users.noreply.github.com>
2025-02-04 10:31:52 -0500

    updated the bubble shooter page

--- games/bubbleshooter/main.js
+++ games/bubbleshooter/main.js
@@ -1,12 +1,38 @@
 var swfobject = {};
+var ruffleInstance = null; // Store the player instance globally
 
-swfobject.embedSWF = function(url, cont, width, height){
-    var ruffle = window.RufflePlayer.newest(),
-        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            width: width,
-            height: height,
-            style: 'width: ' + width + 'px; height: ' + height + 'px',
-        });
+swfobject.embedSWF = function(url, cont, width, height) {
+    var ruffle = window.RufflePlayer.newest();
+    var container = document.getElementById(cont);
 
-    player.load({ url: url });
-}
+    // Remove existing player if it exists
+    if (ruffleInstance) {
+        container.innerHTML = ''; // Clear previous instance
+    }
+
+    ruffleInstance = ruffle.createPlayer();
+    container.appendChild(ruffleInstance);
+
+    // Set initial size
+    ruffleInstance.width = width;
+    ruffleInstance.height = height;
+    ruffleInstance.style.width = width + "px";
+    ruffleInstance.style.height = height + "px";
+
+    ruffleInstance.load({ url: url });
+};
+
+function resizeFlash(width, height) {
+    if (ruffleInstance) {
+        ruffleInstance.width = width;
+        ruffleInstance.height = height;
+        ruffleInstance.style.width = width + "px";
+        ruffleInstance.style.height = height + "px";
+    }
+};
+
+window.addEventListener("message", (event) => {
+    if (event.data.action === "resize") {
+        resizeFlash(event.data.width, event.data.height);
+    }
+});
